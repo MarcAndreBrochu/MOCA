2b9cc0cb17d9209d1f2fb3f191ca1ee9d88e99b0
Marc-André Brochu <marcandrebrochu@icloud.com>
2015-04-03 12:57:45 -0400

    Création des fichiers squelette du projet

---
+++ MOCA/include/MOCA/MOCA.h
@@ -0,0 +1,16 @@
+#ifndef MOCA_H
+#define MOCA_H
+
+#include "Stardust.h"
+#include "World.h"
+#include "AbstractBody.h"
+
+#include "Particle.h"
+#include "Solid.h"
+#include "Ball.h"
+#include "Box.h"
+
+#include "CollisionHandler.h"
+#include "Integrator.h"
+
+#endif // MOCA_H
--- MOCA/src/A.cpp
+++
@@ -1,9 +0,0 @@
-#include "A.h"
-
-A::A(int n) {
-    _n = n;
-}
-
-void A::print() {
-    cout << "Nombre d'okis: " << _n << endl;
-}
