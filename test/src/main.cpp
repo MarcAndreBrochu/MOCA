955c84d4a357ab0fd1154fb7ca20135a95a92fdb
Marc-André Brochu <marcandrebrochu@icloud.com>
2015-04-03 12:59:58 -0400

    Enlevé les références au fichiers de test initiaux

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include <MOCA/A.h>
-#include <MOCA/B.h>
 
 using namespace std;
 
 int main() {
-    
-    A *a = new A(10);
-    a->print();
-    
-    B b(a);
-    b.count();
-    
-    cout << a << endl;
-    delete a;
     
     return EXIT_SUCCESS;
 }
