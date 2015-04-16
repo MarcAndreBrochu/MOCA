1629c3d6c141d4f015bcbd21dc097d80afb97fb5
Marc-André Brochu <marcandrebrochu@icloud.com>
2015-04-15 20:22:36 -0400

    Certains tests avec Qt et MOCA

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <MOCA/MOCA.h>
+#include <QGuiApplication>
+#include <QSurfaceFormat>
+#include <QWindow>
+
+#include <MOCA/MOCA.h>
 
 using namespace std;
 
-int main() {
-    return 0;
+int main(int argc, char **argv) {
+
+    QGuiApplication app(argc, argv);
+    app.setOrganizationName("Marc-André Brochu & Ludovic Bilodeau-Laflamme");
+    app.setApplicationName("MOCA test");
+    app.setApplicationDisplayName(app.applicationName());
+
+    QWindow window;
+    window.resize(640, 480);
+    window.show();
+
+    World world;
+
+    return app.exec();
 }
