8391a83048d65dcb34db75a36cd90dbf7d4d0338
LudovicBL <LudovicBL@users.noreply.github.com>
2015-04-17 12:43:02 -0400

    Implémentation d'un intégrateur de Simpson
    Implémentation de base, nécessitant une fonction de base connue... (à modifier)

--- MOCA/src/Integrator.cpp
+++ MOCA/src/Integrator.cpp
@@ -5,8 +5,22 @@ using namespace Integrator;
 // .........................................................
 // Implementation de la methode de Simpson
 // .........................................................
-Simpson::Simpson() {}
+Simpson::Simpson(int a, int b, int n) {
+    int i = 0;
+    double somme = 0;
+    double h = 0.0;
+    h = (b-a)/static_cast<float>(n);
+    somme = somme + yFonct(a) + yFonct(b);
+    for(i=1;i<n; i=i+2)
+        somme = somme + 4*yFonct(i*h);
+    for(i=2; i<n-1; i=i+2)
+        somme = somme + 2*yFonct(i*h);
+    return somme*h/3;
+}
+Simpson::yFonct(double x) {
+}
 Simpson::~Simpson() {}
+
 
 
 // .........................................................
