3b4a9c3182c0801cf11c1f476852d615e2eb50e8
Marc-André Brochu <marcandrebrochu@icloud.com>
2015-04-21 10:02:26 -0400

    Programme de test [SUCCÈS]

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,6 +5,7 @@
 #include <QWindow>
 
 #include <MOCA/MOCA.h>
+#include <MOCA/Utils.h>
 
 using namespace std;
 
@@ -14,12 +15,28 @@ int main(int argc, char **argv) {
     app.setOrganizationName("Marc-André Brochu & Ludovic Bilodeau-Laflamme");
     app.setApplicationName("MOCA test");
     app.setApplicationDisplayName(app.applicationName());
-
-    QWindow window;
-    window.resize(640, 480);
-    window.show();
     
     World world;
+    Particle particlel;
+    particlel.setMass(1);
+    particlel.setPosition(0, 10, 0);
+    particlel.setVelocity(0, 0, 0);
+
+    arma::vec3 accelGravite;
+    accelGravite[0] = MOCA_METERS_TO_PIXELS(0);
+    accelGravite[1] = MOCA_METERS_TO_PIXELS(-9.8);
+    accelGravite[2] = MOCA_METERS_TO_PIXELS(0);
+
+    world.addBody(&particlel);
+    world.applyAcceleration(accelGravite);
+//    particlel.applyForce(accelGravite);
+
+    double fixedDt = 1.0f / 60.0f;
+    for (int i = 0; i < 600; i++) {
+
+        cout << fixedDt * i << "\t" << particlel.getPosition()[1] << "\t" << particlel.getVelocity()[1] << endl;
+        world.updateWorld(fixedDt);
+    }
 
     return app.exec();
 }
