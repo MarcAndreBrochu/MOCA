1f51ad10013d6eee5830e7e5943adbc262c61918
Marc-André Brochu <marcandrebrochu@icloud.com>
2015-04-21 09:58:40 -0400

    Fonction pour transformer des pixels et mètres

--- MOCA/include/MOCA/Utils.h
+++ MOCA/include/MOCA/Utils.h
@@ -1,4 +1,16 @@
 #ifndef MOCA_UTILS_H
 #define MOCA_UTILS_H
 
+#include <limits>
+
+static const double M_PTM_RATIO = 8; // 1m = 32px
+
+inline double MOCA_METERS_TO_PIXELS(double meters) {
+    return meters * M_PTM_RATIO;
+}
+
+inline bool MOCA_GREATER_THAN_ZERO(double value) {
+    return value > std::numeric_limits<double>::epsilon();
+}
+
 #endif // MOCA_UTILS_H
